049fd4e8e4fac301681bc383f27f312fdd6eeefb
zarkprogame <zarkprogame@gmail.com>
2023-11-18 16:09:35 -0500

    Servicio de Transporte Model

---
+++ Traslado Seguro/Traslado Seguro/Models/ServicioTransporte.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Traslado_Seguro.Models
+{
+	public class ServicioTransporte
+	{
+		public int Id { get; set; }
+
+		[DataType(DataType.Date)]
+		[Display(Name =	"Fecha de Creacion")]
+		public DateTime? Fecha { get; set;}
+		public string NameConductor{ get; set; }
+		public int ClienteId { get; set; }
+		public Cliente Cliente { get; set;}
+	}
+}
