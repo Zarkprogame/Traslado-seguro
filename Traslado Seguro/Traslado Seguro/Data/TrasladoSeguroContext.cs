5843a20af60f6ae642d0467160d8400e0eee9ab3
zarkprogame <zarkprogame@gmail.com>
2023-11-18 16:26:04 -0500

    Migracion de las tablas

--- Traslado Seguro/Traslado Seguro/Data/TrasladoSeguroContext.cs
+++ Traslado Seguro/Traslado Seguro/Data/TrasladoSeguroContext.cs
@@ -5,11 +5,11 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace Traslado_Seguro.Data
 {
-	public class TrasladoSeguroContext: DbContext
+	internal class TrasladoSeguroContext: DbContext
 	{
-		public TrasladoSeguroContext(DbContextOptions options) : base(options)
-		{
-		}
+		//public TrasladoSeguroContext(DbContextOptions options) : base(options)
+		//{
+		//}
 		public DbSet<Conductor> Conductores { get; set; }
 		public DbSet<Cliente> Clientes { get; set; }
 		public DbSet<ServicioTransporte> ServiciosTransportes { get; set; }
@@ -17,7 +17,7 @@ namespace Traslado_Seguro.Data
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer(
-				"Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = TrasladoSeguroDB; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False"
+				"Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = TrasladoSeguroDB; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False"
 				);
 		}
 	}
---
+++ Traslado Seguro/Traslado Seguro/Migrations/20231118212406_Tables.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Traslado_Seguro.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conductores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conductores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiciosTransportes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NameConductor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiciosTransportes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiciosTransportes_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiciosTransportes_ClienteId",
+                table: "ServiciosTransportes",
+                column: "ClienteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conductores");
+
+            migrationBuilder.DropTable(
+                name: "ServiciosTransportes");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
