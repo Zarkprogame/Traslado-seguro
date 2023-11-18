10a2beae5a397761a26a8e79eeff63ec50fb4297
zarkprogame <zarkprogame@gmail.com>
2023-11-18 15:41:09 -0500

    Conductor Model

---
+++ Traslado Seguro/Traslado Seguro/Models/Conductor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Traslado_Seguro.Models
+{
+	public class Conductor
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
