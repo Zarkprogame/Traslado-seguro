6b0e5f081138f59b69d2d85cc97e6f2600b92ae5
zarkprogame <zarkprogame@gmail.com>
2023-11-18 19:56:31 -0500

    Index Servicio de transporte

---
+++ Traslado Seguro/Traslado Seguro/Pages/ServicioTransporte/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Traslado_Seguro.Data;
+using Traslado_Seguro.Models;
+
+namespace Traslado_Seguro.Pages.ServicioTransporte
+{
+    public class IndexModel : PageModel
+    {
+		private readonly TrasladoSeguroContext _context;
+		public IndexModel(TrasladoSeguroContext context)
+		{
+			_context = context;
+		}
+		public IEnumerable<Models.ServicioTransporte> ServiciosTransportes { get; set; }
+		public async Task OnGet()
+		{
+			ServiciosTransportes = await _context.ServiciosTransportes.Include(c => c.Cliente).ToListAsync();
+		}
+	}
+}
