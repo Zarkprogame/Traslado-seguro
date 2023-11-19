8953afd2c66aaef5d7bd3985410c2e2f2d000748
zarkprogame <zarkprogame@gmail.com>
2023-11-18 23:50:54 -0500

    Logout and desauthentication

---
+++ Traslado Seguro/Traslado Seguro/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Traslado_Seguro.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
